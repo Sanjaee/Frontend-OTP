ad7d0d2d1706fdbc94444dda61ba0ca9967e5443
Sanjaee <afrizaahmad18@gmail.com>
2024-09-18 17:28:00 +0700

    maintance dasboard

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -5,6 +5,7 @@ import { DrawerDemo } from "@/components/Chart/DrawerDemo";
 import { DropdownMenuDemo } from "@/components/Menu/DropDown";
 import { useLogin } from "../useLogin";
 import { useToast } from "@/hooks/use-toast";
+import { useEffect } from "react";
 
 const Dashboard = () => {
   useLogin();
@@ -21,7 +22,16 @@ const Dashboard = () => {
     });
   };
 
-  useLogin();
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://www.tiktok.com/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <div className="flex flex-col gap-6 items-center h-[1000px]">
@@ -30,7 +40,21 @@ const Dashboard = () => {
         <DrawerDemo />
         <DropdownMenuDemo />
         <Button onClick={handleLogout}>Logout</Button>
-      </div>
+      </div>{" "}
+      <blockquote
+        data-testid="tiktok-embed"
+        className="tiktok-embed p-4"
+        cite="https://www.tiktok.com/@ahmadafriza25"
+        data-unique-id="ahmadafriza25"
+        data-embed-type="creator"
+        style={{ maxWidth: "780px", minWidth: "288px" }}
+      >
+        <section>
+          <a target="_blank" href="https://www.tiktok.com/@ahmadafriza25">
+            @ahmadafriza25.com
+          </a>
+        </section>
+      </blockquote>
     </div>
   );
 };
